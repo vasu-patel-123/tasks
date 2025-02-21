1d274773a9c52ece2d104e354a049b44c5ba0186
Vasu patel <69396812+vasu-patel-123@users.noreply.github.com>
2025-02-20 23:25:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UM COS420 with React Hooks and TypeScript
+                Vasu Patel
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
